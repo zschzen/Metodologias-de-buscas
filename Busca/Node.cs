4bf2e85097dca87c8895350d83822cb2bc05deca
zschzen <leandro@peres.dev>
2020-03-31 21:10:33 -0300

    TreeStructure
    Adicionado TreeStructure namespace

---
+++ Busca/Node.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace TreeStructure
+{
+
+    /// <summary>
+    /// Classe dedicada aos nódulos da árvore
+    /// Parte dos estudo de Leandro Peres, Março de 2020
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <see cref="https://youtube.com/watch?v=K0-qs--naUo"/>
+    [System.Serializable]
+    public class Node<T>
+    {
+        #region Vars
+
+        public T Data { get; set; }                         // Dado do nódulo
+        public Node<T> Parent { get; set; }                 // Nódulo pai
+        public ICollection<Node<T>> Children { get; set; }  // Lista de nódulos filhos
+
+        #endregion
+
+        /// <summary>
+        /// Construtor da classe Node
+        /// </summary>
+        /// <param name="data">Dado a ser registrado no módulo</param>
+        /// <param name="parent">Nódulo pai. deixar nulo para nódulo raiz</param>
+        public Node(T data, Node<T> parent = null)
+        {
+            this.Data = data;
+            this.Children = new List<Node<T>>();
+            this.Parent = parent;
+        }
+
+        /// <summary>
+        /// Método dedicado à adição de nódulos filhos
+        /// </summary>
+        /// <param name="child">Dados a serem adicionados</param>
+        /// <returns>Nódulo filho criado</returns>
+        public Node<T> AddChild(T child)
+        {
+            Node<T> newChild = new Node<T>(child, this);
+            this.Children.Add(newChild);
+
+            return newChild;
+        }
+
+        /// <summary>
+        /// Partindo do nódulo atual, calcula o tamanho total da árvore
+        /// </summary>
+        /// <returns>Inteiro representando o tamanho da árvore</returns>
+        public int GetHeight()
+        {
+            int height = 1;
+            Node<T> current = this;
+
+            while (current.Parent != null)
+            {
+                height++;
+                current = current.Parent;
+            }
+
+            return height;
+        }
+
+        /// <summary>
+        /// Relata os dados do objeto nódulo como string
+        /// </summary>
+        /// <returns>Dado registrado no nódulo</returns>
+        public override string ToString()
+        {
+            if (this.Data != null)
+            {
+                string strBase = "Node Data = {0}\nParent Data = {1}\nHeight = {2}";
+                return string.Format(strBase, this.Data, this.Parent.Data, this.GetHeight());
+            }
+            else return "[data null]";
+        }
+    }
+
+    /// <summary>
+    /// Classe dedicada para o escopo da árvore de dados
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [System.Serializable]
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+    }
+}
---
+++ Busca/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TreeStructure;
+
+namespace Busca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tree<int> tree = new Tree<int>();
+
+            /*
+            *                25
+            *              /    \
+            *             /      \ 
+            *            20       36
+            *           /  \     /  \    
+            *          10   22  30   \
+            *         /  \      |     40   
+            *        5    12    28   /  \
+            *       / \   |         38  48
+            *      1   8  15           /  \
+            *                        45    50
+            */
+
+            tree.Root = new Node<int>(25);
+
+            Node<int> nodeA = tree.Root.AddChild(20);
+            Node<int> nodeB = tree.Root.AddChild(36);
+
+            Node<int> nodeC = nodeA.AddChild(10);
+            Node<int> nodeD = nodeA.AddChild(22);
+
+            Node<int> nodeE = nodeB.AddChild(30);
+            Node<int> nodeF = nodeB.AddChild(40);
+
+            Node<int> nodeG = nodeC.AddChild(5);
+            Node<int> nodeH = nodeC.AddChild(12);
+
+            Node<int> nodeI = nodeE.AddChild(28);
+
+            Node<int> nodeJ = nodeF.AddChild(38);
+            Node<int> nodeK = nodeF.AddChild(48);
+
+            Node<int> nodeL = nodeG.AddChild(1);
+            Node<int> nodeM = nodeG.AddChild(8);
+
+            Node<int> nodeN = nodeH.AddChild(15);
+
+            Node<int> nodeO = nodeK.AddChild(45);
+            Node<int> nodeP = nodeK.AddChild(50);
+
+            Console.ReadKey();
+        }
+    }
+}
